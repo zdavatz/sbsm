e1aadf199ddea763adc8277a3efb218bd3e22aaa
hwyss <hwyss@gentoo.hwyss.net>
2004-12-10 09:35:17 +0100

    ChangeSet 1.29: enable shortcut-urls lib/sbsm/trans_handler.rb: enable shortcut-urls test/test_trans_handler.rb: enable shortcut-urls

--- lib/sbsm/trans_handler.rb
+++ lib/sbsm/trans_handler.rb
@@ -10,35 +10,23 @@ module SBSM
 			File.dirname(__FILE__))
 		PARSER_PATH = File.expand_path('uri_parser.rb', 
 			File.dirname(__FILE__))
+		SHORTCUT_PATH = '../etc/shortcuts.rb'
+		HANDLER_URI = '/index.rbx'
 		@@empty_check ||= nil
 		@@lang_check ||= nil
 		@@uri_parser ||= nil
-		def canonical_uri(uri)
-			@@uri_parser = self.uri_parser if(@@uri_parser.nil?)
-			ast = @@uri_parser.parse(uri)
-			values = []
-			ast.children_names.each { |name|
-				case name
-				when'language', 'flavor', 'event'
-					values.push([name, ast.send(name).value])
-				when 'variables'
-					ast.variables.each { |pair|
-						key = pair.key.value
-						val = if(pair.children_names.include?('value'))
-							pair.value.value
-						else
-							''
-						end
-						values.push([key, val])
-					}
-				end
-			}
-			parts = [
-				"/index.rbx", 
-				values.collect { |pair| pair.join('=') }.join('&')
-			]
-			parts.delete('')
-			parts.compact.join('?')
+		def handle_shortcut(request)
+			path = File.expand_path(SHORTCUT_PATH, request.server.document_root)
+			path.untaint
+			code = '{' << File.read(path) << '}'
+			code.untaint
+			shortcuts = instance_eval(code)
+			if(notes = shortcuts[request.uri])
+				notes.each { |key, val|
+					request.notes.add(key, val)
+				}
+				request.uri = HANDLER_URI
+			end
 		end
 		def parse_uri(request)
 			@@uri_parser = self.uri_parser if(@@uri_parser.nil?)
@@ -60,22 +48,27 @@ module SBSM
 					}
 				end
 			}
-			parts = [
-				"/index.rbx", 
-				values.collect { |pair| pair.join('=') }.join('&')
-			]
 		end
 		def translate_uri(request)
 			@@empty_check = Regexp.new('^/?$')
 			@@lang_check = Regexp.new('^/[a-z]{2}(/|$)')
+			begin
+				handle_shortcut(request)
+				rescue StandardError => err
+				fmt = 'Notice: unable to load url shortcuts: %s'
+				request.server.log_notice(fmt, err.message)
+				fmt = 'Hint: store shortcuts in Ruby-Code at DOCUMENT_ROOT/%s'
+				request.server.log_notice(fmt, SHORTCUT_PATH)
+				fmt = 'e.g.: "/shortcut" => {"variables" => "to be sent"}'
+				request.server.log_notice(fmt)
+			end
 			uri = request.uri
 			case uri
 			when @@empty_check
-				request.uri = '/index.rbx'
+				request.uri = HANDLER_URI
 			when @@lang_check
 				self.parse_uri(request)
-				request.uri = '/index.rbx'
-				#request.uri = canonical_uri(uri)
+				request.uri = HANDLER_URI
 			end
 			Apache::DECLINED
 		end
@@ -94,9 +87,9 @@ module SBSM
 			require parser_path
 			SBSM._uri_parser
 		end
-		module_function :canonical_uri
 		module_function :translate_uri
 		module_function :parse_uri
 		module_function :uri_parser
+		module_function :handle_shortcut
 	end
 end
--- test/test_trans_handler.rb
+++ test/test_trans_handler.rb
@@ -7,37 +7,173 @@ $:.unshift File.expand_path('../lib', File.dirname(__FILE__))
 require 'test/unit'
 require 'sbsm/trans_handler'
 require 'cgi'
+require 'fileutils'
 
+module Apache
+	DECLINED = 0
+end
 module SBSM
 	class TestTransHandler < Test::Unit::TestCase
-		def test_canonical_uri
-			uri = '/'
-			expected = '/index.rbx'
-			assert_equal(expected, TransHandler.canonical_uri(uri))
-			uri = '/fr'
-			expected = '/index.rbx?language=fr'
-			assert_equal(expected, TransHandler.canonical_uri(uri))
-			uri = '/en/'
-			expected = '/index.rbx?language=en'
-			assert_equal(expected, TransHandler.canonical_uri(uri))
-			uri = '/en/flavor'
-			expected = '/index.rbx?language=en&flavor=flavor'
-			assert_equal(expected, TransHandler.canonical_uri(uri))
-			uri = '/en/other/'
-			expected = '/index.rbx?language=en&flavor=other'
-			assert_equal(expected, TransHandler.canonical_uri(uri))
-			uri = '/de/gcc/search/state_id/407422388/search_query/ponstan'
-			expected = '/index.rbx?language=de&flavor=gcc&event=search&state_id=407422388&search_query=ponstan'
-			assert_equal(expected, TransHandler.canonical_uri(uri))
-			uri = '/de/gcc/search/state_id/407422388/search_query/ponstan/page/4'
-			expected = '/index.rbx?language=de&flavor=gcc&event=search&state_id=407422388&search_query=ponstan&page=4'
-			assert_equal(expected, TransHandler.canonical_uri(uri))
-			uri = '/de/gcc/search/pretty//state_id/407422388/search_query/ponstan/page/4'
+		class RequestStub
+			attr_accessor :uri, :notes, :server
+		end
+		class ServerStub 
+			attr_accessor :document_root
+			def log_notice(fmt, *args)
+			end
+		end
+		class NotesStub < Hash
+			alias :add :store
+		end
+		def setup
+			@doc_root = File.expand_path('data/htdoc', File.dirname(__FILE__))
+			@etc_path = File.expand_path('../etc', @doc_root)
+			@doc_root.taint
+		end
+		def teardown
+			FileUtils.rm_r(@etc_path) if(File.exist?(@etc_path))
+		end
+		def test_translate_uri
+			request = RequestStub.new
+			request.server = ServerStub.new
+
+			request.uri = '/'
+			request.notes = NotesStub.new
+			TransHandler.translate_uri(request)
+			assert_equal({}, request.notes)
+			assert_equal('/index.rbx', request.uri)
+
+			request.uri = '/fr'
+			request.notes = NotesStub.new
+			TransHandler.translate_uri(request)
+			assert_equal({'language' => 'fr'}, request.notes)
+			assert_equal('/index.rbx', request.uri)
+
+			request.uri = '/en/'
+			request.notes = NotesStub.new
+			TransHandler.translate_uri(request)
+			assert_equal({'language' => 'en'}, request.notes)
+			assert_equal('/index.rbx', request.uri)
+
+			request.uri = '/en/flavor'
+			request.notes = NotesStub.new
+			TransHandler.translate_uri(request)
+			expected = {
+				'language'=>	'en',
+				'flavor'	=>	'flavor',
+			}
+			assert_equal(expected, request.notes)
+			assert_equal('/index.rbx', request.uri)
+
+			request.uri = '/en/other'
+			request.notes = NotesStub.new
+			TransHandler.translate_uri(request)
+			expected = {
+				'language'=>	'en',
+				'flavor'	=>	'other',
+			}
+			assert_equal(expected, request.notes)
+			assert_equal('/index.rbx', request.uri)
+
+			request.uri = '/de/gcc/search/state_id/407422388/search_query/ponstan'
+			request.notes = NotesStub.new
+			TransHandler.translate_uri(request)
+			expected = {
+				'language'		=>	'de',
+				'flavor'			=>	'gcc',
+				'event'				=>	'search',
+				'state_id'		=>	'407422388',
+				'search_query'=>	'ponstan',
+			}
+			assert_equal(expected, request.notes)
+			assert_equal('/index.rbx', request.uri)
+
+			request.uri = '/de/gcc/search/state_id/407422388/search_query/ponstan/page/4'
+			request.notes = NotesStub.new
+			TransHandler.translate_uri(request)
+			expected = {
+				'language'		=>	'de',
+				'flavor'			=>	'gcc',
+				'event'				=>	'search',
+				'state_id'		=>	'407422388',
+				'search_query'=>	'ponstan',
+				'page'				=>	'4',
+			}
+			assert_equal(expected, request.notes)
+			assert_equal('/index.rbx', request.uri)
+
+			request.uri = '/de/gcc/search/pretty//state_id/407422388/search_query/ponstan/page/4'
+			request.notes = NotesStub.new
 			expected = '/index.rbx?language=de&flavor=gcc&event=search&pretty=&state_id=407422388&search_query=ponstan&page=4'
-			assert_equal(expected, TransHandler.canonical_uri(uri))
-			uri = '/de/gcc/search/search_query/'
-			expected = '/index.rbx?language=de&flavor=gcc&event=search&search_query='
-			assert_equal(expected, TransHandler.canonical_uri(uri))
+			TransHandler.translate_uri(request)
+			expected = {
+				'language'		=>	'de',
+				'flavor'			=>	'gcc',
+				'event'				=>	'search',
+				'pretty'			=>	'',
+				'state_id'		=>	'407422388',
+				'search_query'=>	'ponstan',
+				'page'				=>	'4',
+			}
+			assert_equal(expected, request.notes)
+			assert_equal('/index.rbx', request.uri)
+
+			request.uri = '/de/gcc/search/search_query/'
+			request.notes = NotesStub.new
+			TransHandler.translate_uri(request)
+			expected = {
+				'language'		=>	'de',
+				'flavor'			=>	'gcc',
+				'event'				=>	'search',
+				'search_query'=>	'',
+			}
+			assert_equal(expected, request.notes)
+			assert_equal('/index.rbx', request.uri)
+		end
+		def test_translate_uri__shortcut
+			request = RequestStub.new
+			request.server = ServerStub.new
+			request.server.document_root = @doc_root
+
+			request.uri = '/shortcut'
+			request.notes = NotesStub.new
+			assert_nothing_raised { 
+				TransHandler.translate_uri(request)
+			}
+			assert_equal({}, request.notes)
+			assert_equal('/shortcut', request.uri)
+			
+			FileUtils.mkdir_p(@etc_path)
+			shortcut = File.join(@etc_path, 'shortcuts.rb')
+			File.open(shortcut, 'w') { |fh|
+				fh.puts <<-EOS
+					'/shortcut'	=>	{	'shortcut'	=>	'variables'},
+					'/somewhere'=>  {	'over'			=>	'the rainbow',
+														'goodbye'		=>	'yellow brick road'},
+				EOS
+			}
+			
+			request.uri = '/shortcut'
+			request.notes = NotesStub.new
+			assert_nothing_raised {
+				# run in safe-mode
+				Thread.new {
+					$SAFE = 1
+					TransHandler.translate_uri(request)
+				}.join
+			}
+			assert_equal({'shortcut' => 'variables'}, request.notes)
+			assert_equal('/index.rbx', request.uri)
+
+			request.uri = '/somewhere'
+			request.notes = NotesStub.new
+			TransHandler.translate_uri(request)
+			expected = {
+				'over'		=>	'the rainbow',
+				'goodbye'	=>	'yellow brick road',
+			}
+			assert_equal(expected, request.notes)
+			assert_equal('/index.rbx', request.uri)
 		end
 	end
 end
