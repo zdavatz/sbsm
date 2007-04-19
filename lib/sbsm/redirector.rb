daba5b338298c2a84f438ee2d41c3f0c5f7a8ea3
Hannes Wyss <hwyss@ywesee.com>
2007-04-19 15:54:51 +0200

    Redirector: redirect if the request-method was not GET, even if events match

--- lib/sbsm/redirector.rb
+++ lib/sbsm/redirector.rb
@@ -14,12 +14,12 @@ module SBSM
       end
     end
     def redirect?
-      #return @state.direct_event && @request_method != 'GET'
       direct = @state.direct_event
       if(direct.is_a?(Array))
         direct = direct.first
       end
-      direct && ![direct, :sort].include?(event)
+      direct && (@request_method != 'GET' \
+                 || ![direct, :sort].include?(event))
     end
     def to_html
       if(redirect?)
