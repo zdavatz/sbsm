07803a3da501c2b0d67fbfe1989d55f132b1e9aa
Masaomi Hatakeyama <mhatakeyama@ywesee.com>
2011-08-22 10:27:44 +0200

    Fix the number of argument error of DRbObject respond_to method

--- lib/sbsm/drb.rb
+++ lib/sbsm/drb.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
+# DRb::DRbObject -- fix bug respond_to argument -- 22.08.2011 -- mhatakeyama@ywesee.com
 # DRb::DRbObject -- fix bug in ruby -- 23.09.2005 -- hwyss@ywesee.com
 
 require 'drb'
 
 module DRb
 	class DRbObject
-		def respond_to?(msg_id)
+		def respond_to?(msg_id, *args)
 			case msg_id
 			when :_dump
 				true
